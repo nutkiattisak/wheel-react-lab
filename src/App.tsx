ac45f54a1b89a456f8dbc29e416dfc025cc2b475
Kiattisak Chantharamaneechote <32291012+nutkiattisak@users.noreply.github.com>
2024-05-27 23:44:04 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import WheelOfFortune from './components/WheelOfFortune'
+
+function App() {
+  return (
+    <>
+      <WheelOfFortune />
+    </>
+  )
+}
+
+export default App
